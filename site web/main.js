c9ef2e6be981c2fef9ef15d68af7a5f18d7399ed
islemsassi19 <salmasassi170@gmail.com>
2023-03-06 17:27:30 +0100

    first push

---
+++ site web/main.js
@@ -0,0 +1,13 @@
+var ch=document.getElementById('first1').value
+var num=document.getElementById('').value
+function alphabetique(){
+if(ch1==""){
+    alert("verifier le nom est vide")
+}
+if (ch!=="A")&&(ch!=="Z"){
+    alert ("Verifier votre nom")
+}
+}
+function numerique(){
+
+}
